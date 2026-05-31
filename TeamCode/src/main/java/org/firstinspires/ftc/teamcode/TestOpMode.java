bff9ca4e188a91be9b97e935988d34173d07d6e8
STEAMPowered12873 <steampowered12873@gmail.com>
2026-05-31 16:04:36 -0400

    test fr and fl motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,6 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class TestOpMode {
+@TeleOp(name="test")
+public class TestOpMode extends OpMode {
+
+    private DcMotor flMotor = null;
+    private DcMotor frMotor = null;
+
+    @Override
+    public void init() {
+        flMotor = hardwareMap.get(DcMotor.class, "flMotor");
+        frMotor = hardwareMap.get(DcMotor.class, "frMotor");
+        flMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        double flPower;
+        double frPower;
+
+        double yMovement = gamepad1.right_stick_y;
+        double xMovement = gamepad1.right_stick_x;
+
+        flPower = yMovement+xMovement;
+        frPower = yMovement-xMovement;
+
+        flMotor.setPower(flPower);
+        frMotor.setPower(frPower);
+    }
 }
