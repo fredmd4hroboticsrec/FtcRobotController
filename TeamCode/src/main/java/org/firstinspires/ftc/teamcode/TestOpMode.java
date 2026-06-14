7cdfddedbe1ed47a8b8ffc92b6cebb0664453dae
STEAMPowered12873 <steampowered12873@gmail.com>
2026-06-14 16:15:30 -0400

    added bl and br motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -9,27 +9,45 @@ public class TestOpMode extends OpMode {
 
     private DcMotor flMotor = null;
     private DcMotor frMotor = null;
+    private DcMotor blMotor = null;
+    private DcMotor brMotor = null;
 
     @Override
     public void init() {
         flMotor = hardwareMap.get(DcMotor.class, "flMotor");
         frMotor = hardwareMap.get(DcMotor.class, "frMotor");
-        flMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        flMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frMotor.setDirection(DcMotorSimple.Direction.REVERSE );
+        blMotor = hardwareMap.get (DcMotor.class, "blMotor");
+        brMotor = hardwareMap.get(DcMotor.class, "brMotor");
+        blMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        brMotor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void loop() {
         double flPower;
         double frPower;
+        double blPower;
+        double brPower;
 
         double yMovement = gamepad1.right_stick_y;
         double xMovement = gamepad1.right_stick_x;
 
         flPower = yMovement+xMovement;
         frPower = yMovement-xMovement;
+        blPower = yMovement+xMovement;
+        brPower = yMovement-xMovement;
 
         flMotor.setPower(flPower);
         frMotor.setPower(frPower);
+        blMotor.setPower(blPower);
+        brMotor.setPower(brPower);
+
+
+
+
+
+
     }
 }
