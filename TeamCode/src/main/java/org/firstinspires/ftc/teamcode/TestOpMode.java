f24008228c067ec164a029429e8809fd75ec68b0
STEAMPowered12873 <steampowered12873@gmail.com>
2026-07-12 16:55:40 -0400

    Added adjustable weights and rectified driving direction errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -11,17 +11,21 @@ public class TestOpMode extends OpMode {
     private DcMotor frMotor = null;
     private DcMotor blMotor = null;
     private DcMotor brMotor = null;
-
+    private double flWeight = 1;
+    private double frWeight = 1;
+    private double blWeight = 1;
+    private double brWeight = 1;
+    private boolean change = true;
     @Override
     public void init() {
         flMotor = hardwareMap.get(DcMotor.class, "flMotor");
         frMotor = hardwareMap.get(DcMotor.class, "frMotor");
-        flMotor.setDirection(DcMotorSimple.Direction.FORWARD);
-        frMotor.setDirection(DcMotorSimple.Direction.REVERSE );
+        flMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         blMotor = hardwareMap.get (DcMotor.class, "blMotor");
         brMotor = hardwareMap.get(DcMotor.class, "brMotor");
-        blMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        brMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        blMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        brMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
@@ -34,20 +38,31 @@ public class TestOpMode extends OpMode {
         double yMovement = gamepad1.right_stick_y;
         double xMovement = gamepad1.right_stick_x;
 
-        flPower = yMovement+xMovement;
-        frPower = yMovement-xMovement;
-        blPower = yMovement+xMovement;
-        brPower = yMovement-xMovement;
+        flPower = yMovement-xMovement;
+        frPower = yMovement+xMovement;
+        blPower = -yMovement-xMovement;
+        brPower = -yMovement+xMovement;
 
-        flMotor.setPower(flPower);
-        frMotor.setPower(frPower);
-        blMotor.setPower(blPower);
-        brMotor.setPower(brPower);
+        flMotor.setPower(flPower*flWeight);
+        frMotor.setPower(frPower*frWeight);
+        blMotor.setPower(blPower*blWeight);
+        brMotor.setPower(brPower*brWeight);
+        if (change) {
+            flWeight += ((gamepad1.dpad_up ? 0.01 : 0) - (gamepad1.dpad_down ? 0.01 : 0)) * (gamepad1.x ? 1 : 0);
+            frWeight += ((gamepad1.dpad_up ? 0.01 : 0) - (gamepad1.dpad_down ? 0.01 : 0)) * (gamepad1.y ? 1 : 0);
+            blWeight += ((gamepad1.dpad_up ? 0.01 : 0) - (gamepad1.dpad_down ? 0.01 : 0)) * (gamepad1.a ? 1 : 0);
+            brWeight += ((gamepad1.dpad_up ? 0.01 : 0) - (gamepad1.dpad_down ? 0.01 : 0)) * (gamepad1.b ? 1 : 0);
+            change = false;
+        }
+        if (!gamepad1.dpad_up & !gamepad1.dpad_down) {
+            change = true;
+        }
+        telemetry.addData("Weights:","FL:%.2f, FR:%.2f, BL:%.2f, BR:%.2f",flWeight,frWeight,blWeight,brWeight);
 
+    }
 
-
-
-
-
+    @Override
+    public void stop() {
+        telemetry.addData("Weights:","FL:%.2f, FR:%.2f, BL:%.2f, BR:%.2f",flWeight,frWeight,blWeight,brWeight);
     }
 }
